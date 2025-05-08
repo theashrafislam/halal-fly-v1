0eecdfb87e52852dc8e72cf17e6fccd80920b939
Ashraful Islam <theashrafislam@gmail.com>
2025-05-08 12:44:43 +0600

    create what to expect section

---
+++ src/app/Components/Home/WhatToExpectSection/WhatToExpectSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const WhatToExpectSection = () => {
+    return (
+        <div className='bg-[#10111A] '>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div></div>
+                <div>
+                    <h5>What To Expect From Us</h5>
+                    <h3>Travel with Faith, Explore with Peace!
+                        We Provide the best Umrah and Hajj Experience
+                    </h3>
+                    <p>Laboris anim duis sunt aliquip tempor qui. Est in nulla eiusmod ad aliqua laboris jugni lol occaecat excepteur voluptate. Eiusmod enim ea minim occaecat in occaecat occaecat. Adipisicing velit aute duis pariatur minim officia officia velit nostrud ipsum ullamco anim aute sint reprehenderit nulla enim.</p>
+                    {/* vison and mission */}
+                    <div>
+                        <p>Eiusmod enim ea minim occaecat in occaecat occaecat. Adipisicing velit aute duis.</p>
+                        <p>Eiusmod enim ea minim occaecat in occaecat occaecat. Adipisicing velit aute duis.</p>
+                    </div>
+                    <p>Laboris anim duis sunt aliquip tempor qui. Est in nulla eiusmod ad aliqua laboris jugni lol occaecat excepteur voluptate.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WhatToExpectSection;
