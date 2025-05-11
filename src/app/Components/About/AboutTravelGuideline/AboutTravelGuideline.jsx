19853895e24e9e6e8018dcd7e6777fee5095e180
Ashraful Islam <theashrafislam@gmail.com>
2025-05-11 17:00:44 +0600

    about travel guideline component is complate

---
+++ src/app/Components/About/AboutTravelGuideline/AboutTravelGuideline.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import FlightIcon from '../../Packages/FlightIcon';
+import { BsAirplaneEngines } from 'react-icons/bs';
+
+const AboutTravelGuideline = () => {
+    return (
+        <div
+            className="relative bg-[#000000] bg-cover bg-center bg-no-repeat h-[600px] text-white"
+            style={{ backgroundImage: "url('/images/travel-bg.png')" }}
+        >
+            <div className="space-y-4 text-start max-w-5xl mx-auto pt-20">
+                <h6 className='text-[#D0A148] capitalize font-philosopher text-xl'>welcome to our store.</h6>
+                <h2 className="font-bold mb-4 text-[#D0A148] font-philosopher text-3xl md:text-4xl">Discover Our Travel <br /> Guideline</h2>
+                <p className="text-lg text-[#FFF]">
+                    Explore essential travel tips and guidelines <br /> for your next adventure. Make your journey <br /> safe and unforgettable.
+                </p>
+                <p className="text-lg text-[#FFF]">
+                    Explore essential travel tips and guidelines for <br /> your next adventure. Make your journey <br /> safe and unforgettable.
+                </p>
+                <button className='capitalize btn-primary mt-7'>contact us</button>
+            </div>
+            {/* absolute  */}
+            <div className='absolute p-4 rounded-lg bg-[#D0A148] right-3/12 top-20 flex flex-col items-center justify-center text-[#FFF]'>
+                <BsAirplaneEngines className="w-16 pb-3 h-16"/>
+                <h5 className='text-6xl font-bold font-philosopher'>10M</h5>
+                <p className='text-base font-bold font-philosopher'>Different Parts</p>
+            </div>
+        </div>
+    );
+};
+
+export default AboutTravelGuideline;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import AboutHeaderSection from '../Components/About/AboutHeaderSection/AboutHeaderSection';
+import AboutTravelGuideline from '../Components/About/AboutTravelGuideline/AboutTravelGuideline';
 
 const AboutPage = () => {
     return (
         <div>
             {/* about header section  */}
             <AboutHeaderSection />
+            
+            {/* About Travel Guideline */}
+            <AboutTravelGuideline />
         </div>
     );
 };
