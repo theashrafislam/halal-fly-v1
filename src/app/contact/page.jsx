883ec0d1cc2052c45a5bc18202ac2e71972fdd7d
Ashraful Islam <theashrafislam@gmail.com>
2025-05-17 16:09:48 +0600

    make need help box also contact infomation added in contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import ContactHeaderSection from '../Components/Contact/ContactHeaderSection';
+import ContactHeaderSection from '../Components/Contact/ContactHeaderSection/ContactHeaderSection';
+import GetInTouchSection from '../Components/Contact/GetInTouchSection/GetInTouchSection';
 
 const ContactUs = () => {
     return (
         <div>
             <div>
+                {/* contact header section  */}
                 <ContactHeaderSection />
+
+                {/* black image section hehe  */}
+                <div className='w-full h-[300px] bg-[#000000]'>
+
+                </div>
+
+                {/* main contect of contact section  */}
+                <div className='max-w-6xl mx-auto'>
+                    <GetInTouchSection />
+                </div>
             </div>
         </div>
     );
